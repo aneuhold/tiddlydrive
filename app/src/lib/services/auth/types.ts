36fdc336a9937e12735e2eaa332bf029da8b2856
Anton G Neuhold Jr <agneuhold@gmail.com>
2025-09-27 17:43:17 -0700

    Fix up error handling

--- app/src/lib/services/auth/types.ts
+++ app/src/lib/services/auth/types.ts
@@ -7,7 +7,7 @@ export type AccessTokenResponse = {
 
 export type StoredToken = {
   accessToken: string;
-  /** epoch millis */
+  /** epoch milliseconds */
   expireAt: number;
   scope: string;
 };
@@ -17,5 +17,11 @@ export enum AuthErrorCode {
   TokenFailed = 'token_failed',
   ScopeNotGranted = 'scope_not_granted',
   GapiUnavailable = 'gapi_unavailable',
-  SsrUnavailable = 'ssr_unavailable'
+  SsrUnavailable = 'ssr_unavailable',
+  /**
+   * Indicates that access was blocked by an admin policy, e.g. because the
+   * user's account is not allowed to access the app / something else.
+   * See https://developers.google.com/identity/protocols/oauth2/web-server#authorization-errors-admin-policy-enforced
+   */
+  AccessRestrictedByPolicy = 'access_restricted_by_policy'
 }
